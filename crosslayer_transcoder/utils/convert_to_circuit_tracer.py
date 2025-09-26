8774ddd6547d898ea936441cec2e88428e878df1
jiito <benjamin.allanrahill@gmail.com>
2025-09-26 09:04:56 -0700

    successful integration test

--- crosslayer_transcoder/utils/convert_to_circuit_tracer.py
+++ crosslayer_transcoder/utils/convert_to_circuit_tracer.py
@@ -1,17 +1,25 @@
 # simple file to convert the lightning model to a circuit-tracer model
-from typing import Union
+import os
+from pathlib import Path
+from typing import List, Union
 
-from einops import einops
+from huggingface_hub import upload_folder
 import torch
+import yaml
+from einops import einops
+from safetensors.torch import save_file
 from torch.export.graph_signature import OutputKind
 
-from crosslayer_transcoder.model.clt import CrosslayerDecoder
+from crosslayer_transcoder.model.clt import (
+    CrosslayerDecoder,
+    Encoder,
+    SimpleCrossLayerTranscoder,
+)
 from crosslayer_transcoder.model.clt_lightning import (
     CrossLayerTranscoderModule,
     JumpReLUCrossLayerTranscoderModule,
     TopKCrossLayerTranscoderModule,
 )
-
 
 CLTModule = Union[
     CrossLayerTranscoderModule,
@@ -20,54 +28,168 @@ CLTModule = Union[
 ]
 
 
-def convert_decoder(decoder: CrosslayerDecoder):
-    # TODO: convert the CLT to a circuit tracing CLT shape
-    # our decoder shape: [i+1, d_features, d_acts]
-    # expected W_{i} shape: [d_features, n_layers - i, d_acts]
-    # From [here](https://github.com/safety-research/circuit-tracer/blob/2eff952df66400eeb1066595dc5567a7203c6acd/circuit_tracer/transcoder/cross_layer_transcoder.py#L103C38-L103C73)
-    #
+def add_decoder_bias(d: List[dict], decoder: CrosslayerDecoder):
+    for i in range(decoder.n_layers):
+        d[i][f"b_dec_{i}"] = (
+            decoder.b[i].cpu() if hasattr(decoder, "b") else torch.zeros(decoder.d_acts)
+        )
+    return d
 
-    d_feats = decoder.d_features
-    d_acts = decoder.d_acts
-    n_layers = decoder.n_layers
 
-    # layer i decoder mat of shape [d_feats, n_layers - i, d_acts]
-    decoder_dict = {}
+def save_decoder_dict(decoder_dict: dict, path: str):
+    for key, value in decoder_dict.items():
+        save_file({key: value}, f"{path}/{key}.safetensors")
 
-    for i in range(n_layers):
-        output_dec_i = torch.zeros([d_feats, n_layers - i, d_acts])
 
-        for k in range(i, n_layers):
+def convert_model_to_circuit_tracer(
+    lightning_module: CLTModule,
+    save_dir: str,
+    feature_input_hook: str = "blocks.{layer}.hook_resid_pre",
+    feature_output_hook: str = "blocks.{layer}.hook_mlp_out",
+):
+    # convert the lightning model to the circuit-tracer compatible shape
+    save_path = Path(save_dir)
+    save_path.mkdir(parents=True, exist_ok=True)
+
+    encoder = lightning_module.model.encoder
+    decoder = lightning_module.model.decoder
+    n_layers = encoder.n_layers
+    d_acts = encoder.d_acts  # -> d_model
+    d_features = encoder.d_features  # -> d_transcoder
+
+    for source_layer in range(encoder.n_layers):
+        # encoder
+        layer_encoder_dict = {
+            f"W_enc_{source_layer}": encoder.W[source_layer]
+            .T.contiguous()
+            .cpu(),  # Transpose!
+            f"b_enc_{source_layer}": (
+                encoder.b[source_layer].cpu()
+                if hasattr(encoder, "b")
+                else torch.zeros(encoder.d_features)
+            ),
+            f"b_dec_{source_layer}": (
+                decoder.b[source_layer].cpu()
+                if hasattr(decoder, "b")
+                else torch.zeros(d_acts)
+            ),
+            # TODO: add non-lin params like threshold
+        }
+
+        # TODO: check names
+        save_file(layer_encoder_dict, f"{save_path}/W_enc_{source_layer}.safetensors")
+
+        # decoder
+        output_dec_i = torch.zeros([d_features, n_layers - source_layer, d_acts])
+
+        for k in range(source_layer, n_layers):
             # get decoder mat for layer i --> k
             decoder_w_k = decoder.get_parameter(f"W_{k}")
-            dec_i_k = decoder_w_k[i, ...]
+            dec_i_k = decoder_w_k[source_layer, ...]
             assert dec_i_k.shape == (
-                d_feats,
+                d_features,
                 d_acts,
             )
-            output_dec_i[:, k - i, ...] = dec_i_k
+            output_dec_i[:, k - source_layer, ...] = dec_i_k.cpu()
 
-        decoder_dict[f"W_dec_{i}"] = output_dec_i
+        decoder_dict = {f"W_dec_{source_layer}": output_dec_i}
 
-    return decoder_dict
+        save_file(decoder_dict, f"{save_path}/W_dec_{source_layer}.safetensors")
+
+    # Create config
+    config = {
+        "model_kind": "cross_layer_transcoder",
+        "feature_input_hook": feature_input_hook,
+        "feature_output_hook": feature_output_hook,
+    }
+
+    with open(save_path / "config.yaml", "w") as f:
+        yaml.dump(config, f, default_flow_style=False)
 
 
-def convert_encoder():
-    pass
+def upload_to_hub(save_dir: str, repo_id: str, repo_type: str = "model"):
+    from huggingface_hub import upload_folder
 
-
-# def convert_model_to_circuit_tracer(lightning_module: CLTModule):
-def convert_model_to_circuit_tracer():
-    # convert the lightning model to the circuit-tracer compatible shape
-
-    mock_decoder = CrosslayerDecoder(728, 1023, 12)
-
-    ct_decoder_dict = convert_decoder(mock_decoder)
-
-    print(len(ct_decoder_dict))
-    print(ct_decoder_dict["W_dec_0"].shape)
-    pass
+    print(f"Uploading {save_dir} to {repo_id} ({repo_type})")
+    upload_folder(folder_path=save_dir, repo_id=repo_id, repo_type=repo_type)
 
 
 if __name__ == "__main__":
-    convert_model_to_circuit_tracer()
+    from crosslayer_transcoder.model.clt_lightning import CrossLayerTranscoderModule
+    from crosslayer_transcoder.model.clt import (
+        CrossLayerTranscoder,
+        Encoder,
+        CrosslayerDecoder,
+    )
+    from crosslayer_transcoder.model.jumprelu import JumpReLU
+    from crosslayer_transcoder.model.standardize import (
+        DimensionwiseInputStandardizer,
+        DimensionwiseOutputStandardizer,
+    )
+    from crosslayer_transcoder.metrics.replacement_model_accuracy import (
+        ReplacementModelAccuracy,
+    )
+    from crosslayer_transcoder.metrics.dead_features import DeadFeatures
+
+    # Create components based on default.yaml config
+    encoder = Encoder(d_acts=768, d_features=10_000, n_layers=12)
+
+    decoder = CrosslayerDecoder(d_acts=768, d_features=10_000, n_layers=12)
+
+    nonlinearity = JumpReLU(theta=0.03, bandwidth=0.01, n_layers=12, d_features=10_000)
+
+    input_standardizer = DimensionwiseInputStandardizer(n_layers=12, activation_dim=768)
+
+    output_standardizer = DimensionwiseOutputStandardizer(
+        n_layers=12, activation_dim=768
+    )
+
+    model = CrossLayerTranscoder(
+        encoder=encoder,
+        decoder=decoder,
+        nonlinearity=nonlinearity,
+        input_standardizer=input_standardizer,
+        output_standardizer=output_standardizer,
+    )
+
+    # replacement_model = ReplacementModelAccuracy(
+    #     model_name="openai-community/gpt2", device_map="mps", loader_batch_size=2
+    # )
+
+    dead_features = DeadFeatures(
+        n_features=10_000,
+        n_layers=12,
+        return_per_layer=True,
+        return_log_freqs=True,
+        return_neuron_indices=True,
+    )
+
+    clt_module = CrossLayerTranscoderModule(
+        model=model,
+        # replacement_model=replacement_model,
+        dead_features=dead_features,
+        learning_rate=1e-4,
+        compile=True,
+        lr_decay_step=80_000,
+        lr_decay_factor=0.1,
+        compute_dead_features=True,
+        compute_dead_features_every=500,
+    )
+
+    convert_model_to_circuit_tracer(clt_module, "clt_module")
+
+    upload_folder(
+        folder_path="clt_module", repo_id="jiito/clt_test_gpt2_zero", repo_type="model"
+    )
+    upload_folder(
+        folder_path="clt_module/encoder",
+        path_in_repo="encoder",
+        repo_id="jiito/clt_test_gpt2_zero",
+        repo_type="model",
+    )
+    upload_folder(
+        folder_path="clt_module/decoder",
+        path_in_repo="decoder",
+        repo_id="jiito/clt_test_gpt2_zero",
+        repo_type="model",
+    )
