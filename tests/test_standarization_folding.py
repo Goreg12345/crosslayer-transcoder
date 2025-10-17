459acd701e899fe3cded4f68bfa9297ec18967fe
jiito <benjamin.allanrahill@gmail.com>
2025-10-17 11:45:26 -0700

    fix: numerical stability

--- tests/test_standarization_folding.py
+++ tests/test_standarization_folding.py
@@ -66,6 +66,50 @@ def test_math_sanity_check():
     assert_allclose(lhs, rhs)
 
 
+def test_math_sanity_check():
+    batch_size, d_acts, d_feats, n_layers = 100, 768, 32, 12
+    torch.manual_seed(42)
+    dtype = torch.float64
+    batch = torch.randn((batch_size, 2, n_layers, d_acts), dtype=dtype)
+    encoder = Encoder(d_acts=d_acts, d_features=d_feats, n_layers=n_layers).to(dtype)
+
+    input_std = DimensionwiseInputStandardizer(
+        n_layers=n_layers, activation_dim=d_acts
+    ).to(dtype)
+    input_std.initialize_from_batch(batch=batch)
+
+    resid = batch[:, 0]
+    W = encoder.W
+    b = encoder.b
+    mean, std = input_std.mean, input_std.std
+
+    W_div_std = W / std.unsqueeze(-1)
+
+    lhs = (
+        einsum(
+            (resid - mean) / std,
+            W,
+            "batch n_layers d_acts, n_layers d_acts d_features -> batch n_layers d_features",
+        )
+        + b
+    )
+    rhs = (
+        einsum(
+            resid,
+            W_div_std,
+            "batch n_layers d_acts, n_layers d_acts d_features -> batch n_layers d_features",
+        )
+        + b
+        - einsum(
+            mean,
+            W_div_std,
+            "n_layers d_acts, n_layers d_acts d_features -> n_layers d_features",
+        )
+    )
+
+    assert torch.allclose(lhs, rhs, rtol=1e-7, atol=1e-9)
+
+
 def test_encoder_standarization_folding():
     encoder = Encoder(d_acts=D_ACTS, d_features=D_FEATS, n_layers=N_LAYERS).to(DTYPE)
     input_std = DimensionwiseInputStandardizer(
