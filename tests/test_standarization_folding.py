422b1d2d423c05b3a79147b955141487b12bafed
jiito <benjamin.allanrahill@gmail.com>
2025-10-17 12:51:05 -0700

    add const, rm comments

--- tests/test_standarization_folding.py
+++ tests/test_standarization_folding.py
@@ -9,19 +9,32 @@ from crosslayer_transcoder.model.standardize import (
 )
 
 
+torch.manual_seed(42)
+DTYPE = torch.float64
+BATCH_SIZE, D_ACTS, D_FEATS, N_LAYERS = 100, 768, 32, 12
+BATCH = torch.randn((BATCH_SIZE, 2, N_LAYERS, D_ACTS), dtype=DTYPE)
+
+
+def assert_allclose(lhs, rhs, rtol=1e-7, atol=1e-9):
+    try:
+        assert torch.allclose(lhs, rhs, rtol=rtol, atol=atol)
+    except AssertionError as e:
+        print("lhs shape:", lhs.shape)
+        print("rhs shape:", rhs.shape)
+        print("max diff:", (lhs - rhs).max().item())
+        print("mean diff:", (lhs - rhs).mean().item())
+        raise e from e
+
+
 def test_math_sanity_check():
-    batch_size, d_acts, d_feats, n_layers = 100, 768, 32, 12
-    torch.manual_seed(42)
-    dtype = torch.float64
-    batch = torch.randn((batch_size, 2, n_layers, d_acts), dtype=dtype)
-    encoder = Encoder(d_acts=d_acts, d_features=d_feats, n_layers=n_layers).to(dtype)
+    encoder = Encoder(d_acts=D_ACTS, d_features=D_FEATS, n_layers=N_LAYERS).to(DTYPE)
 
     input_std = DimensionwiseInputStandardizer(
-        n_layers=n_layers, activation_dim=d_acts
-    ).to(dtype)
-    input_std.initialize_from_batch(batch=batch)
+        n_layers=N_LAYERS, activation_dim=D_ACTS
+    ).to(DTYPE)
+    input_std.initialize_from_batch(batch=BATCH)
 
-    resid = batch[:, 0]
+    resid = BATCH[:, 0]
     W = encoder.W
     b = encoder.b
     mean, std = input_std.mean, input_std.std
@@ -50,42 +63,24 @@ def test_math_sanity_check():
         )
     )
 
-    assert torch.allclose(lhs, rhs, rtol=1e-7, atol=1e-9)
+    assert_allclose(lhs, rhs)
 
 
 def test_encoder_standarization_folding():
-    # create test tensors
-
-    batch_size = 100
-    d_acts = 768
-    d_feats = 32
-    n_layers = 12
-
-    torch.manual_seed(42)
-
-    dtype = torch.float64
-
-    batch = torch.randn((batch_size, 2, n_layers, d_acts), dtype=dtype)
-
-    encoder = Encoder(d_acts=d_acts, d_features=d_feats, n_layers=n_layers).to(dtype)
+    encoder = Encoder(d_acts=D_ACTS, d_features=D_FEATS, n_layers=N_LAYERS).to(DTYPE)
     input_std = DimensionwiseInputStandardizer(
-        n_layers=n_layers, activation_dim=d_acts
-    ).to(dtype)
+        n_layers=N_LAYERS, activation_dim=D_ACTS
+    ).to(DTYPE)
 
-    input_std.initialize_from_batch(batch=batch)
+    input_std.initialize_from_batch(batch=BATCH)
 
-    # run tensors through the orignal forward passes
-
-    resid = batch[:, 0]
+    resid = BATCH[:, 0]
 
     acts = input_std(resid)
 
     pre_actvs = encoder(acts)
 
-    # fold in
-
     w_enc_folded, b_enc_folded = input_std.fold_in_encoder(encoder.W, encoder.b)
-    # run inputs with folded
 
     folded_params = {
         "W": torch.nn.Parameter(w_enc_folded.clone().detach()),
@@ -93,48 +88,27 @@ def test_encoder_standarization_folding():
     }
     pre_actvs_folded = functional_call(encoder, folded_params, resid, {"layer": "all"})
 
-    diff = pre_actvs - pre_actvs_folded
-    print("max diff:", diff.max().item(), "mean diff:", diff.mean().item())
-
-    # test equality
-    assert torch.allclose(pre_actvs, pre_actvs_folded, rtol=1e-7, atol=1e-9)
+    assert_allclose(pre_actvs, pre_actvs_folded)
 
 
 def test_decoder_standarization_folding():
-    # create test tensors
-
-    batch_size = 100
-    d_acts = 768
-    d_feats = 32
-    n_layers = 12
-
-    torch.manual_seed(42)
-
-    dtype = torch.float64
-
-    encoder_batch = torch.randn((batch_size, 2, n_layers, d_acts), dtype=dtype)
-
     decoder = CrosslayerDecoder(
-        d_acts=d_acts, d_features=d_feats, n_layers=n_layers
-    ).to(dtype)
+        d_acts=D_ACTS, d_features=D_FEATS, n_layers=N_LAYERS
+    ).to(DTYPE)
     output_std = DimensionwiseOutputStandardizer(
-        n_layers=n_layers, activation_dim=d_acts
-    ).to(dtype)
+        n_layers=N_LAYERS, activation_dim=D_ACTS
+    ).to(DTYPE)
 
-    output_std.initialize_from_batch(batch=encoder_batch)
+    output_std.initialize_from_batch(batch=BATCH)
 
-    # run tensors through the orignal forward passes
-
-    test_features = torch.randn((batch_size, n_layers, d_feats), dtype=dtype)
+    test_features = torch.randn((BATCH_SIZE, N_LAYERS, D_FEATS), dtype=DTYPE)
 
     recons_norm = decoder(test_features)
 
     recons = output_std(recons_norm)
 
-    # fold in
-
     folded_params = {}
-    for layer in range(n_layers):
+    for layer in range(N_LAYERS):
         w_dec_folded = output_std.fold_in_decoder_weights_layer(
             decoder.get_parameter(f"W_{layer}"),
             layer,
@@ -142,18 +116,11 @@ def test_decoder_standarization_folding():
         assert w_dec_folded.shape == decoder.get_parameter(f"W_{layer}").shape
         folded_params[f"W_{layer}"] = torch.nn.Parameter(w_dec_folded.clone().detach())
 
-    # fold bias
     b_dec_folded = decoder.b * output_std.std + output_std.mean
     folded_params["b"] = torch.nn.Parameter(b_dec_folded.clone().detach())
-
-    print("folded_params:", folded_params.keys())
 
     recons_folded = functional_call(
         decoder, folded_params, test_features, {"layer": "all"}
     )
 
-    diff = recons - recons_folded
-    print("max diff:", diff.max().item(), "mean diff:", diff.mean().item())
-
-    # test equality
-    assert torch.allclose(recons, recons_folded, rtol=1e-7, atol=1e-9)
+    assert_allclose(recons, recons_folded)
