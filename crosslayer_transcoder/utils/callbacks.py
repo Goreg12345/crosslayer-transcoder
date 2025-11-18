8652e3a1eb8922bcff8cb250639dbc75abb82290
jiito <benjamin.allanrahill@gmail.com>
2025-11-18 05:53:42 +0000

    test: config of hook points for callback

--- crosslayer_transcoder/utils/callbacks.py
+++ crosslayer_transcoder/utils/callbacks.py
@@ -61,29 +61,32 @@ class EndOfTrainingCheckpointCallback(L.Callback):
         checkpoint_path = self.checkpoint_dir / "clt.ckpt"
         trainer.save_checkpoint(checkpoint_path)
 
-
 class ModelConversionCallback(L.Callback):
     """Callback to convert the model to a circuit-tracer model."""
 
-    def __init__(self, save_dir: str = "clt_module", kinds=["circuit_tracer"]):
+    # Note: you can't type these with List
+    def __init__(self, save_dir: str = "clt_module", kinds=["circuit_tracer"], on_event=["on_train_batch_end"]):
         super().__init__()
         self.save_dir = Path(save_dir)
         self.kinds = kinds
+        self.on_event = on_event
         self.converters = {
             "circuit_tracer": CircuitTracerConverter,
         }
+        self._setup_hooks()
 
-    def _convert_model(self, pl_module):
+    def _setup_hooks(self):
+        for event in self.on_event:
+            setattr(self, event, lambda *args, **kwargs: self._convert_model(*args, **kwargs))
+
+    # Note: this should have the signature as all Lightning callbacks
+    def _convert_model(self, trainer, pl_module, **kwargs):
         for kind in self.kinds:
             converter = self.converters[kind](save_dir=self.save_dir.as_posix())
             converter.convert_and_save(pl_module)
             logger.info(
                 f"{kind} model converted and saved to {self.save_dir.as_posix()}"
             )
-
-    # TODO: for testing, just turn on the train batch end
-    def on_train_batch_end(self, trainer, pl_module):
-        self._convert_model(pl_module)
 
 
 class HuggingFaceCallback(L.Callback):
