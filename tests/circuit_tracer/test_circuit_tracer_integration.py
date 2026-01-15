40d1291d16c95a5a30263505a74d2276ad6d7a83
jiito <benjamin.allanrahill@gmail.com>
2026-01-15 14:38:49 -0800

    add relu error test

--- tests/circuit_tracer/test_circuit_tracer_integration.py
+++ tests/circuit_tracer/test_circuit_tracer_integration.py
@@ -1,8 +1,10 @@
 import pathlib
 import shutil
 from unittest.mock import MagicMock
+import torch
 import yaml
 from circuit_tracer.transcoder.cross_layer_transcoder import load_clt
+import pytest
 
 from crosslayer_transcoder.model.clt import CrossLayerTranscoder
 from crosslayer_transcoder.utils.model_converters.circuit_tracer import (
@@ -47,3 +49,50 @@ def test_circuit_tracer_integration():
 
     # cleanup
     shutil.rmtree(save_dir)
+
+
+def test_topk_nonlinearity():
+    model_config = {
+        "class_path": "crosslayer_transcoder.model.clt.CrossLayerTranscoder",
+        "init_args": {
+            "encoder": {
+                "class_path": "crosslayer_transcoder.model.clt.Encoder",
+                "init_args": {"d_acts": 768, "d_features": 10_000, "n_layers": 12},
+            },
+            "decoder": {
+                "class_path": "crosslayer_transcoder.model.clt.CrosslayerDecoder",
+                "init_args": {"d_acts": 768, "d_features": 10_000, "n_layers": 12},
+            },
+            "nonlinearity": {
+                "class_path": "crosslayer_transcoder.model.topk.PerLayerTopK",
+                "init_args": {
+                    "k": 8,
+                },
+            },
+            "input_standardizer": {
+                "class_path": "crosslayer_transcoder.model.standardize.DimensionwiseInputStandardizer",
+                "init_args": {"n_layers": 12, "activation_dim": 768},
+            },
+            "output_standardizer": {
+                "class_path": "crosslayer_transcoder.model.standardize.DimensionwiseOutputStandardizer",
+                "init_args": {"n_layers": 12, "activation_dim": 768},
+            },
+        },
+    }
+    clt_model = CrossLayerTranscoder.from_config(model_config)
+    clt_module = MagicMock()
+    clt_module.model = clt_model
+    save_dir = pathlib.Path("topk_clt_module_test")
+    feature_input_hook = "hook_resid_mid"
+    feature_output_hook = "hook_mlp_out"
+
+    with pytest.raises(
+        ValueError,
+        match="TopK nonlinearity is not supported by circuit-tracer.",
+    ):
+        converter = CircuitTracerConverter(
+            save_dir=save_dir,
+            feature_input_hook=feature_input_hook,
+            feature_output_hook=feature_output_hook,
+        )
+        converter.convert_and_save(clt_module)
